0dab590a41b857a058ebb7e858c22a692e9807b8
pomelo925 <yoseph.huang@gmail.com>
2025-03-19 23:56:55 +0800

    optim: simple entrypoint

--- run.sh
+++ run.sh
@@ -11,15 +11,17 @@ sudo chmod +x docker/entrypoint/*
 
 # Check validity of args
 if [ "$#" -ne 1 ]; then
+  echo ""
   echo "Usage: $0 {raw|build|cali|teleop|cam|sync|train|deploy}"
-  echo "raw\t- Launch container without running any service"
-  echo "build\t- Build ros2 workspace"
-  echo "cali\t- Calibrate Koch Robot Arm"
-  echo "teleop\t- Teleoperate Koch Robot Arm"
-  echo "cam\t- Launch all camera nodes"
-  echo "sync\t- teleop + cam + synchorize related topics"
-  echo "train\t- Train with custom dataset"
-  echo "deploy\t- Deploy trained model"
+  echo ""
+  echo - raw$'\t\t'Launch container without running any service
+  echo - build$'\t\t'Build ros2 workspace
+  echo - cali$'\t\t'Calibrate Koch Robot Arm
+  echo - teleop$'\t'Teleoperate Koch Robot Arm
+  echo - cam$'\t\t'Launch all camera nodes
+  echo - sync$'\t\t'teleop + cam + synchronize related topics
+  echo - train$'\t\t'Train with custom dataset
+  echo - deploy$'\t'Deploy trained model
   exit 1
 fi
 
